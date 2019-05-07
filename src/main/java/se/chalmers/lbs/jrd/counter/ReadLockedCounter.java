2c33710ca5aeb683f65b23ebf75de179dc6073f3
Niclas Alexandersson <alniniclas@gmail.com>
2019-05-07 20:35:47 +0200

    Added thread-safe and non-thread-safe counter benchmarks using read-write locks.

---
+++ src/main/java/se/chalmers/lbs/jrd/counter/ReadLockedCounter.java
@@ -0,0 +1,41 @@
+package se.chalmers.lbs.jrd.counter;
+
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+@ThreadSafe
+public class ReadLockedCounter implements Counter {
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    @GuardedBy("lock")
+    private long count;
+
+    public ReadLockedCounter(long count) {
+        this.count = count;
+    }
+
+    public ReadLockedCounter() {
+        this(0);
+    }
+
+    @Override
+    public long get() {
+        lock.readLock().lock();
+        try {
+            return count;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public long getAndIncrement() {
+        lock.readLock().lock();
+        try {
+            return count++;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+}
